c0d79bfafea445d942df14cd2ca2f677ec02dadd
Janos Jarecsni <janos.jarecsni@gmail.com>
2020-09-03 06:03:10 +0100

    S1L1 completed

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,71 @@
 <page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+    <actionBar title="Meeteor" />
+    <stackLayout>
+        <label class="header">Your Groups</label>
+        <scrollView orientation="horizontal" scrollbarIndicatorVisible={false}>
+            <stackLayout orientation="horizontal" class="groupContainer">
+                {#each myGroups as group}
+                    <stackLayout class="groupPanel">
+                        <image 
+                            src={group.thumbnail}
+                            class="groupThumbnail"
+                        >
+                            {group.thumbnail}
+                        </image>
+                        <label
+                            textWrap={true}
+                            class="groupLabel"
+                        >
+                            {group.name}
+                        </label>
+                    </stackLayout>
+                {/each}
+            </stackLayout>
+        </scrollView>
+    </stackLayout>
 </page>
 
 <script>
-    let message = "Blank Svelte Native App"
+
+    const myGroups = [
+        {
+            name: 'Sunday Book Club',
+            thumbnail: 'https://bookofaces.files.wordpress.com/2019/02/ck-jhbsun081fshcr.jpg?w=256&h=256&crop=1'
+        },
+        {
+            name: 'Svelte Natives',
+            thumbnail: 'https://twobluecommunications.com/wp-content/uploads/2018/12/Chelsea-Waterfront-3-256x256.jpg'
+        },
+        {
+            name: 'Svelte Stars',
+            thumbnail: 'https://twobluecommunications.com/wp-content/uploads/2018/06/Eastward-view-cropped-bottom-downsized-256x256.jpg'
+        },
+        {
+            name: 'Svelte Life',
+            thumbnail: 'https://twobluecommunications.com/wp-content/uploads/2018/06/Eastward-view-cropped-bottom-downsized-256x256.jpg'
+        }
+    ];
 </script>
 
 <style>
-    .info .fas {
-        color: #3A53FF;
+    .header {
+        font-size: 24;
+        margin: 10;
     }
-    .info {
-        font-size: 20;
+    .groupContainer {
+        height: 200;
+        margin: 2;
+    }
+    .groupPanel {
+        background-color: lightgray;
+        margin: 4;
+    }
+    .groupThumbnail {
+        width: 100;
+    }
+    .groupLabel {
+        margin-left: 4;
+        width: 100;
+        height: 50;
     }
 </style>
