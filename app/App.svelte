b4cf4bd8a123901493cd844827e851328523ba0e
Janos Jarecsni <janos.jarecsni@gmail.com>
2020-09-15 06:07:51 +0100

    Lesson 3

--- app/App.svelte
+++ app/App.svelte
@@ -22,11 +22,36 @@
                 {/each}
             </stackLayout>
         </scrollView>
+        <label class="header">Your Calendar</label>
+        <segmentedBar class="calendar"
+            selectedIndex={currentCalendar}
+            on:selectedIndexChange={onCalendarTypeChange}
+        >
+            <segmentedBarItem title="Your Groups"/>
+            <segmentedBarItem title="Going"/>
+            <segmentedBarItem title="Past Events"/>
+        </segmentedBar>
+        {#if currentCalendar === 0}
+            <scrollView scrollbarIndicatorVisible={false}>
+                <stackLayout>
+                    {#each upcomingEvents as event}
+                        <label class="eventLabel">{event.date}</label>
+                        <label class="eventLabel">{event.time}</label>
+                        <label class="eventLabel">{event.title}</label>
+                        <label class="eventLabel last">{event.group}</label>
+                    {/each}
+                </stackLayout>
+            </scrollView>
+        {:else if currentCalendar === 1}
+            <label margin=10>Events you booked</label>
+        {:else}
+            <label margin=10>Past events</label>
+        {/if}
+
     </stackLayout>
 </page>
 
 <script>
-
     const myGroups = [
         {
             name: 'Sunday Book Club',
@@ -45,6 +70,33 @@
             thumbnail: 'https://twobluecommunications.com/wp-content/uploads/2018/06/Eastward-view-cropped-bottom-downsized-256x256.jpg'
         }
     ];
+
+    let currentCalendar = 0;
+    function onCalendarTypeChange(e) {
+        currentCalendar = e.value;
+    }
+
+    let upcomingEvents = [
+        {
+            date: '18/06/2020',
+            time: '16:00',
+            title: 'Some interesting event',
+            group: 'React London'
+        },
+        {
+            date: '18/06/2020',
+            time: '18:00',
+            title: 'Why Svelte is the Best?',
+            group: 'Svelte Hackers'
+        },
+        {
+            date: '19/06/2020',
+            time: '18:00',
+            title: 'Templating with Svelte',
+            group: 'Svelte Rookies'
+        }
+    ];
+    
 </script>
 
 <style>
@@ -67,5 +119,15 @@
         margin-left: 4;
         width: 100;
         height: 50;
+    }
+    .calendar {
+        margin: 5;
+        color: white;
+    }
+    .eventLabel {
+        margin-left: 10;
+    }
+    .eventLabel.last {
+        margin-bottom: 10;
     }
 </style>
